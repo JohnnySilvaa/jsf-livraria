5e6ca6ddd8502e473e90fd4e669bda0ce398d0c7
Johnny Silva <johnnysilva_estagio@hotmail.com>
2018-10-31 15:54:14 -0300

    Conversão e validação de dados

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -2,8 +2,12 @@ package br.com.caelum.livraria.bean;
 
 import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Autor;
@@ -16,8 +20,7 @@ public class LivroBean {
 	private Livro livro = new Livro();
 
 	private Integer autorId;
-	
-	
+
 	public Integer getAutorId() {
 		return autorId;
 	}
@@ -34,26 +37,35 @@ public class LivroBean {
 		System.out.println("Gravando livro " + this.livro.getTitulo());
 
 		if (livro.getAutores().isEmpty()) {
-			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
+			FacesContext.getCurrentInstance().addMessage("autor",
+					new FacesMessage("Livro deve ter pelo menos um Autor"));
+			return;
+		} else {
+			new DAO<Livro>(Livro.class).adiciona(this.livro);
+			this.livro = new Livro();
 		}
-
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
 	}
 
 	public List<Autor> getAutores() {
 		return new DAO<Autor>(Autor.class).listaTodos();
 	}
 
-	
 	public void gravarAutor() {
 		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
 		this.livro.adicionaAutor(autor);
-		
+
 		System.out.println("Autor: " + autor.getNome() + " gravado");
 	}
-	
-	
-	public List<Autor> getAutoresDoLivro(){
+
+	public List<Autor> getAutoresDoLivro() {
 		return this.livro.getAutores();
+	}
+
+	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
+
+		String valor = value.toString();
+		if (!valor.startsWith("1")) {
+			throw new ValidatorException(new FacesMessage("Deveria começar com 1"));
+		}
 	}
 }
